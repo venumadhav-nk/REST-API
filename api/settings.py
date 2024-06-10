969474ea3e06551543e5e4b5e5471517682437b2
venumadha-nk <venumadhav577@gmail.com>
2024-06-10 16:33:13 +0530

    made some changes in files

--- api/settings.py
+++ api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aplication',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- aplication/views.py
+++ aplication/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['POST','GET'])
+def welcome(request):
+    data= request.data
+    print(data)
+    return Response('hi this is nandhakumar how are you ?')

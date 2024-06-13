1e9f3e5638af4f88b4816a77a75dd574a151077b
venumadha-nk <venumadhav577@gmail.com>
2024-06-13 13:10:03 +0530

    created PUT,PATCH,DELETE

--- aplication/urls.py
+++ aplication/urls.py
@@ -3,5 +3,11 @@ from.import views
 
 urlpatterns = [
     path('welcome/',views.welcome),
-    path('get/',views.getemployee)
+    path('get/',views.getemployee),
+    path('add/',views.addEmployee),
+    path('put/',views.putemployee),
+    path('patch/',views.patchemployee),
+    path('delete/',views.delemployee),
+
+
 ]
--- aplication/views.py
+++ aplication/views.py
@@ -20,3 +20,42 @@ def getemployee(request):
     return Response(serializer.data)
 
   
+@api_view(['POST'])
+def addEmployee(request):
+   newEmployee = request.data
+   serializer= EmployeeSerializers(data=newEmployee, many=True)
+   if serializer.is_valid():
+       serializer.save()
+       return Response(f"new employee successfully added{serializer.data}")
+   return Response(serializer.errors)
+
+@api_view(['PUT'])  
+def putemployee(request):
+    empdata= request.data
+    oldemp=Employee.objects.get(id=empdata['id'])
+    serializer=EmployeeSerializers(oldemp,data=empdata)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(f'Employee successfully replaced{serializer.data}')
+    return Response(serializer.errors)
+
+@api_view(['PATCH'])
+def patchemployee(request):
+    empdata= request.data
+    oldemp=Employee.objects.get(id=empdata['id'])
+    serializer=EmployeeSerializers(oldemp,data=empdata,partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(f'Employee successfully modified{serializer.data}')
+    return Response(serializer.errors)
+
+
+@api_view(['DELETE'])
+def delemployee(request):
+    empdata=request.data
+    old=Employee.objects.get(id=empdata['id']).delete()
+    return Response("Employee successfully Deleted")
+
+    
+
+
